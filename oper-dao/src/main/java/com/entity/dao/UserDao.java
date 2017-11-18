f955b027bcb402000bc6bfc974f2f542a731c940
yemingfeng <yemingfeng@dayuwuxian.com>
2017-11-18 22:06:40 +0800

    redesign code instruct

---
+++ oper-dao/src/main/java/com/entity/dao/UserDao.java
@@ -0,0 +1,22 @@
+package com.entity.dao;
+
+
+import com.oper.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author yemingfeng
+ */
+@Repository
+public interface UserDao extends JpaRepository<User, Long> {
+
+  @Query("SELECT user FROM User user WHERE user.email = :email")
+  User getByEmail(@Param("email") String email);
+
+  @Query("SELECT user FROM User user WHERE user.nickname = :nickname")
+  User getByNickname(@Param("nickname") String nickname);
+
+}
--- src/main/java/com/oper/constants/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.oper.constants;
-
-/**
- * @author yemingfeng
- */
-public interface Constants {
-
-}
